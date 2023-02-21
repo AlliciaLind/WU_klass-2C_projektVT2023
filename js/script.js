d4bb6e70eebffdf0f18fcd4e10262d4765cd0613
Alicia Lind <alicia.lind@elev.ga.ntig.se>
2023-02-21 14:51:21 +0100

    Fixed header, added more content

--- js/script.js
+++ js/script.js
@@ -1,15 +1,17 @@
-const burgerMenu = document.getElementById('hamburger-menu')
+const burgerMenuIcon = document.getElementById('hamburger-menu')
+const closeMenuIcon = document.getElementById('close-menu')
 const navBar = document.getElementById('nav-bar')
-const closeMenu = document.getElementById('close-menu')
 
 function openMenu() {
     navBar.classList.add('nav-visible')
+    document.body.style.overflow = "hidden"
 }
 
 function closeMenu() {
     navBar.classList.remove('nav-visible')
+    document.body.style.overflow = "visible"
+    
 }
 
-
-burgerMenu.addEventListener('click', openMenu)
-closeMenu.addEventListener('click', closeMenu)
+burgerMenuIcon.addEventListener('click', openMenu)
+closeMenuIcon.addEventListener('click', closeMenu)
